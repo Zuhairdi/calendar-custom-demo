b677074a960a709857085211d670e93a651b1776
Ahmad Zuhairdi Noh <ahmadzuhairdi@yahoo.com>
2021-08-22 17:31:48 +0800

    custom-calendar-demo

---
+++ android/app/src/main/java/gaialight/technology/calendar_custom_demo/MainActivity.java
@@ -0,0 +1,6 @@
+package gaialight.technology.calendar_custom_demo;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+void main() async {
+  await initializeDateFormatting();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TableCalendar(
+                calendarBuilders: CalendarBuilders(
+                  defaultBuilder: (context, day, focusedDay) {
+                    //you need to change a locale 'ar' for arabic to any format you desire
+                    var format = DateFormat.d('ar');
+                    var dateString = format.format(day);
+                    return Center(
+                      child: Text(
+                        '$dateString',
+                      ),
+                    );
+                  },
+                ),
+                firstDay: DateTime.utc(2010, 10, 16),
+                lastDay: DateTime.utc(2030, 3, 14),
+                focusedDay: DateTime.now(),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
